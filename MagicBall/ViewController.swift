2be53e2c9f288c55bb8850149e61f58ac011c2c8
Jacek Krzyżanowski <krzyzakov@gmail.com>
2018-11-03 13:28:14 +0100

    start pelna wersja

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tablicaKul = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var losowyNumer: Int = 0
+    
+    @IBOutlet weak var obrazekKuli: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        losujObrazek()
     }
 
-
+    @IBAction func przyciskSpytaj(_ sender: UIButton) {
+        losujObrazek()
+    }
+    
+    func losujObrazek(){
+        losowyNumer = Int.random(in: 0...4)
+        obrazekKuli.image = UIImage.init(named: tablicaKul[losowyNumer])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        losujObrazek()
+    }
 }
 
